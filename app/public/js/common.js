5239e2b9fadb6edae1f4e36a72bd487edecbddaa
omatsuri <matsuoshohei46@gmail.com>
2019-03-06 20:52:15 +0900

    add

--- app/public/js/common.js
+++ app/public/js/common.js
@@ -29,59 +29,9 @@ jQuery(function(){
    })
 });
 
+// 募集要項 アコーディオン
 jQuery(function(){
-   jQuery('.btn1').click(function(){
-       if (jQuery('.btn1').hasClass("serviceOn")) {
-      //activeだったら
-    } else if (!jQuery('.btn1').hasClass("serviceOn")) {
-      //activeじゃなかったら
-      jQuery('.btn1').addClass("serviceOn");
-      jQuery('.btn2').removeClass("serviceOn");
-      jQuery('.btn3').removeClass("serviceOn");
-      jQuery('.content1').addClass("isServiceActive");
-      jQuery('.content2').removeClass("isServiceActive");
-      jQuery('.content3').removeClass("isServiceActive");
-    }
+   jQuery('.accordion').click(function(){
+       jQuery(this).toggleClass('is-open');
    })
-});
-
-jQuery(function(){
-   jQuery('.btn2').click(function(){
-       if (jQuery('.btn2').hasClass("serviceOn")) {
-      //activeだったら
-  } else if (!jQuery('.btn2').hasClass("serviceOn")) {
-      //activeじゃなかったら
-      jQuery('.btn2').addClass("serviceOn");
-      jQuery('.btn1').removeClass("serviceOn");
-      jQuery('.btn3').removeClass("serviceOn");
-      jQuery('.content2').addClass("isServiceActive");
-      jQuery('.content1').removeClass("isServiceActive");
-      jQuery('.content3').removeClass("isServiceActive");
-    }
-   })
-});
-
-jQuery(function(){
-   jQuery('.btn3').click(function(){
-       if (jQuery('.btn3').hasClass("serviceOn")) {
-      //activeだったら
-  } else if (!jQuery('.btn3').hasClass("serviceOn")) {
-      //activeじゃなかったら
-      jQuery('.btn3').addClass("serviceOn");
-      jQuery('.btn1').removeClass("serviceOn");
-      jQuery('.btn2').removeClass("serviceOn");
-      jQuery('.content3').addClass("isServiceActive");
-      jQuery('.content1').removeClass("isServiceActive");
-      jQuery('.content2').removeClass("isServiceActive");
-    }
-   })
-});
-
-jQuery(function(){
-  jQuery(".type").t({
-  // ここにオプションを指定する
-    speed : 30,
-    speed_vary : false,
-    caret : false
-  });
 });
